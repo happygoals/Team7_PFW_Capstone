598c2f1fc11c087d40373ab42d79186629d7a140
heinmon13 <heinmon13@gmail.com>
2018-11-27 01:45:56 -0500

    Added more items for backend. "npm run server" run back end

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,39 @@
 //back end - express should connect to the database or server here.
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express')
+const http = require('http')
+const path = require('path')
+const bodyParser = require('body-parser')
 
-const app = express();
+const app = express()
+const connection = request('express-myconnection')
+const mysql = require('mysql')
 
+app.set('port', process.env.PORT || 4300);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(express.static(path.join(__dirname, 'public')));
+// development only
+if ('development' == app.get('env')) {
+  app.use(express.errorHandler());
+}
+app.use(
+  connection(mysql,{
+    host: 'localhost',
+    user: 'root',
+    password : '123456789',
+    port : 3306, //port mysql
+    database:'sampledata'
+  },'pool')
+)
 
-app.use((req, res, next) => {
-  console.log("first 1");
-  console.log(req.method);
-  console.log(req.url)
-  next();
-});
+app.use(app.router)
 
-module.exports = app;
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'))
+})
+
+module.exports = app
