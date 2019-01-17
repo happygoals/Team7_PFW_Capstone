1c932a0959ec6633e74eed5a0e53f14012a83972
Zarni Mon <heinmon13@gmail.com>
2019-01-16 22:24:39 -0500

    Adding angular routing for population page and linked into population text in header

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { PopulationComponent } from './body/population/population.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: 'population', component: PopulationComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
