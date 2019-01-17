1b1af0dc6bfe9d671e9a46dcb3e7d9e15fbc9bc6
Haemin Ryu <happygoals@naver.com>
2019-01-16 22:40:33 -0500

    Set the app-routing for the link button on the navigation bar (Population page, Routing page, Settings page)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PopulationComponent } from './body/population/population.component';
+import { RoutingComponent } from './body/routing/routing.component';
+import { SettingsComponent } from './body/settings/settings.component';
 
 
 const routes: Routes = [
-  { path: 'population', component: PopulationComponent}
-
+  { path: 'population', component: PopulationComponent},
+  { path: 'routing', component: RoutingComponent},
+  { path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
