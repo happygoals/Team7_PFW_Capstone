e4de9238b13a83ee87bd439f3a29ca63d55926d3
Haemin Ryu <happygoals@naver.com>
2019-01-17 11:24:17 -0500

    Create the link for each pages on the routing module(navbar routing link)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PopulationComponent } from './core/body/population/population.component';
+import { PopulationComponent } from 'src/app/core/body/population/population.component';
+import { RoutingComponent } from 'src/app/core/body/routing/routing.component';
+import { SettingsComponent } from 'src/app/core/body/settings/settings.component';
 
 
 const routes: Routes = [
-  { path: 'population', component: PopulationComponent}
+  { path: 'population', component: PopulationComponent},
+  { path: 'routing', component: RoutingComponent},
+  { path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
