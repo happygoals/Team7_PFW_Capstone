7784131f402ca9ac505da668795dcca62633cc1e
Haemin Ryu <happygoals@naver.com>
2019-02-07 13:56:15 -0500

    create range slider(ng5-slider) with time values on time picker

--- src/app/core/body/routing/routing.component.ts
+++ src/app/core/body/routing/routing.component.ts
@@ -1,5 +1,5 @@
 import { Component} from '@angular/core';
-
+import { Options } from 'ng5-slider';
 
 @Component({
   selector: 'app-core-body-routing',
@@ -8,4 +8,56 @@ import { Component} from '@angular/core';
 })
 
 export class RoutingComponent {
+  minValue: number = 0;
+  maxValue: number = 24;
+  options: Options = {
+    ceil: 24,
+    floor: 0,
+    showSelectionBar: true,
+    stepsArray: [
+      {value: 12, legend: 'AM'},
+      {value: 1},
+      {value: 2},
+      {value: 3},
+      {value: 4},
+      {value: 5},
+      {value: 6},
+      {value: 7},
+      {value: 8},
+      {value: 9},
+      {value: 10},
+      {value: 11},
+      {value: 12, legend: 'PM'},
+      {value: 1},
+      {value: 2},
+      {value: 3},
+      {value: 4},
+      {value: 5},
+      {value: 6},
+      {value: 7},
+      {value: 8},
+      {value: 9},
+      {value: 10},
+      {value: 11},
+      {value: 12, legend: 'AM'}
+    ],
+    getTickColor: (value: number): string => {
+      if (value < 3) {
+        return 'red';
+      }
+      if (value < 7) {
+        return 'orange';
+      }
+      if (value < 10) {
+        return 'yellow';
+      }
+      return '#2AE02A';
+    },
+    selectionBarGradient: {
+      from: 'white',
+      to: 'green',
+    },
+    showTicks: true,
+    draggableRange: true
+  };
 }
