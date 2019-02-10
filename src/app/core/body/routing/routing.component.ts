8ac8d14665c1befead739ea4dcb6d124ceeae050
Haemin Ryu <happygoals@naver.com>
2019-02-10 18:39:04 -0500

    change time slider's time value for AM and PM, create label text for the time picker

--- src/app/core/body/routing/routing.component.ts
+++ src/app/core/body/routing/routing.component.ts
@@ -1,22 +1,60 @@
 import { Component} from '@angular/core';
-import { Options } from 'ng5-slider';
+import { Options, LabelType } from 'ng5-slider';
 
 @Component({
   selector: 'app-core-body-routing',
   templateUrl: './routing.component.html',
-  styleUrls: ['./routing.component.css']
+  styleUrls: ['./routing.component.css','./routing.component.scss']
 })
 
 export class RoutingComponent {
   minValue: number = 0;
   maxValue: number = 24;
   options: Options = {
-    ceil: 24,
+    ceil: 12,
     floor: 0,
+    step: 24,
+    translate: (value: number, label: LabelType): string => {
+      var time = null;
+      var hours = value;
+
+      switch (label) {
+        case LabelType.Low:
+         if (hours < 12) {
+           time = "AM";
+         }
+         else{
+           time = "PM";
+         }
+         if (value == 0) {
+          hours = 12;
+        }
+        if (hours > 12) {
+            hours = hours - 12;
+        }
+          return '<b>Start</b> ' + hours + time;
+        case LabelType.High:
+          if (hours < 12 || hours == 24) {
+            time = "AM";
+          }
+          else{
+            time = "PM";
+          }
+          if (value == 0) {
+            hours = 12;
+          }
+          if (hours > 12) {
+              hours = hours - 12;
+          }
+          return '<b>End</b> ' + hours + time;
+        default:
+          return hours + time;
+      }
+    },
     showSelectionBar: true,
     stepsArray: [
-      {value: 12, legend: 'AM'},
-      {value: 1},
+      {value: 0},
+      {value: 1, legend: 'AM'},
       {value: 2},
       {value: 3},
       {value: 4},
@@ -28,36 +66,26 @@ export class RoutingComponent {
       {value: 10},
       {value: 11},
       {value: 12, legend: 'PM'},
-      {value: 1},
-      {value: 2},
-      {value: 3},
-      {value: 4},
-      {value: 5},
-      {value: 6},
-      {value: 7},
-      {value: 8},
-      {value: 9},
-      {value: 10},
-      {value: 11},
-      {value: 12, legend: 'AM'}
+      {value: 13},
+      {value: 14},
+      {value: 15},
+      {value: 16},
+      {value: 17},
+      {value: 18},
+      {value: 19},
+      {value: 20},
+      {value: 21},
+      {value: 22},
+      {value: 23},
+      {value: 24, legend: 'AM'}
     ],
-    getTickColor: (value: number): string => {
-      if (value < 3) {
-        return 'red';
-      }
-      if (value < 7) {
-        return 'orange';
-      }
-      if (value < 10) {
-        return 'yellow';
-      }
-      return '#2AE02A';
-    },
     selectionBarGradient: {
-      from: 'white',
-      to: 'green',
+      from: 'black',
+      to: 'green', 
+
     },
     showTicks: true,
-    draggableRange: true
+    draggableRange: true,
+    
   };
 }
