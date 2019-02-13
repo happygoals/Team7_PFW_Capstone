a46c5320ec01a68630c8cee529798a1923b2ab98
MinNamgung <ccindy9966@gmail.com>
2019-02-12 20:19:49 -0500

    push push

--- backend/models/beacon.js
+++ backend/models/beacon.js
@@ -1,17 +1,26 @@
 var connection = require('../connection')
 
+var obj = new Array()
+obj = ["a","b"]
+
 var beacon = {
     getAllDataSet:function(callback){
         return connection.query("SELECT * FROM test", callback)
     },
+
     getBeaconsByDateTime(startDate, endDate, startTime, endTime){
-        return connection.query("SELECT beacon FROM test WHERE Date BETWEEN ? AND ?  AND Time BETWEEN ? AND ?", [startDate, endDate, startTime, endTime], (error, rows) => {
-            if (!error)
-              console.log(JSON.stringify(rows))
+        return connection.query("SELECT beacon FROM test WHERE Date BETWEEN ? AND ?  AND Time BETWEEN ? AND ?",
+         [startDate, endDate, startTime, endTime], (error, rows) => {
+
+          if (!error){
+              obj = JSON.stringify(rows)
+              console.log(obj)
+            }
             else
               console.log("error")
         })
     },
+
     getBeaconsById(ID){
         return connection.query("SELECT beacon FROM test WHERE ID=?", [ID], (error, rows) => {
             if (!error)
@@ -21,4 +30,6 @@ var beacon = {
         })
     }
 }
+
+//console.log(obj)
 module.exports = beacon
