d9192e34555070b95c853cb00e9bd5bdf092b093
Haemin Ryu <happygoals@naver.com>
2019-02-19 10:51:20 -0500

    resizing image and chart

--- src/app/core/body/routing/routing.component.ts
+++ src/app/core/body/routing/routing.component.ts
@@ -21,6 +21,10 @@ export class RoutingComponent implements OnInit{
     .subscribe((data : Beacon[]) =>{
       this.beacons = data;
       console.log(this.beacons[1]);
+
+
+
+      
     });
   }
 
