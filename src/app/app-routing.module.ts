c2327e8bf2da63b4b3cb49def54a86c6f4ee8f98
Haemin Ryu <happygoals@naver.com>
2019-02-27 00:48:27 -0500

    create home component & put header for population, routing, and setting page & change the rounting module and link

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { PopulationComponent } from 'src/app/core/body/population/population.component';
 import { RoutingComponent } from 'src/app/core/body/routing/routing.component';
 import { SettingsComponent } from 'src/app/core/body/settings/settings.component';
+import { HomeComponent } from 'src/app/core/body/home/home.component';
 
 
 const routes: Routes = [
   { path: 'population', component: PopulationComponent},
   { path: 'routing', component: RoutingComponent},
-  { path: 'settings', component: SettingsComponent}
+  { path: 'settings', component: SettingsComponent},
+  { path: 'home', component: HomeComponent}
 ];
 
 @NgModule({
