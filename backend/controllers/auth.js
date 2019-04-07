cbea8ee3b29eb26f0794776338c387a61f50b4e4
MinNamgung <ccindy9966@gmail.com>
2019-04-07 17:42:55 -0400

    back-end register/updateProfile/Login done

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -18,6 +18,7 @@ router.get('/', function(request, response){
   response.sendFile(path.join())
 })
 
+//Register
 router.post('/register', (req, res) => {
   let Firstname = req.body.Firstname
   let Lastname = req.body.Lastname
@@ -32,46 +33,70 @@ router.post('/register', (req, res) => {
       return res.status(500).send(err)
     }
     if (result.length > 0) {
-      let addquery = "INSERT INTO `test.login` (Firstname, Lastname, Email, Password) VALUES ('" +
-      Firstname + "', '" + Lastname + "', '" + Email + "', '" + Password+ "')";
-      connection.query(addquery, (err, result) => {
-      if (err) {
-          return res.status(500).send(err);
-      }
-      //res.redirect('/');
-      console.log("created")
-    })
+
+      let stmt ="UPDATE test.login set Firstname ='"+Firstname+"', Lastname ='"+Lastname+"', Email = '"+Email+"', Password = '"+Password+"' WHERE (EmployeeID = '" + EmployeeID + "')"
+         let todo = ["Insert", false]
+         connection.query(stmt,todo, (err,results,fields) => {
+           if(err) {
+             return console.error(err.message)
+           }
+           console.log ('todo id:' + results.insertid)
+         })
   }})
+})
+
+//login
+router.post('/login', (req, res) => {
+  let Email = req.body.Email
+  let Password = req.body.Password
+  let checkingquery = "SELECT * FROM test.login WHERE Email= '" + Email + "' and Password = '"+ Password +"' "
+
+	if (Email && Password) {
+		connection.query(checkingquery, [Email, Password], function(error, results, fields) {
+			if (results.length > 0) {
+				request.session.loggedin = true;
+				request.session.username = username;
+				response.redirect('/home');
+			} else {
+				response.send('Incorrect Username and/or Password!');
+			}
+			response.end();
+		});
+	} else {
+		response.send('Please enter Username and Password!');
+		response.end();
+	}
 })
 
 
 
 
-router.post('/auth/:Email/:Password', function(req, response) {
-/*
-  var Email = request.body.Email;
-  var Password = request.body.Password;
-*/
-  var Email = req.params.Email
-  var Password = req.params.Password
 
-	if (Email && Password) {
-		connection.query('SELECT * FROM test.login WHERE Email = ? AND Password = ?', [Email, Password], function(error, results, fields) {
-			if (results.length > 0) {
-				//request.session.loggedin = true;
-        //request.session.Email = Email;
-        response.redirect('/population');
-        return true;
-			} else {
-				response.send('Incorrect Email and/or Password!');
-        return false;
-      }
-			response.end();
-		})
-	} else {
-		response.send('Please enter Email and Password!');
-		response.end();
-	}
-});
+// router.post('/auth/:Email/:Password', function(req, response) {
+// /*
+//   var Email = request.body.Email;
+//   var Password = request.body.Password;
+// */
+//   var Email = req.params.Email
+//   var Password = req.params.Password
+
+// 	if (Email && Password) {
+// 		connection.query('SELECT * FROM test.login WHERE Email = ? AND Password = ?', [Email, Password], function(error, results, fields) {
+// 			if (results.length > 0) {
+// 				//request.session.loggedin = true;
+//         //request.session.Email = Email;
+//         response.redirect('/population');
+//         return true;
+// 			} else {
+// 				response.send('Incorrect Email and/or Password!');
+//         return false;
+//       }
+// 			response.end();
+// 		})
+// 	} else {
+// 		response.send('Please enter Email and Password!');
+// 		response.end();
+// 	}
+// });
 
 module.exports = router
